f139ac0b77f6110dec415c04d131b40c702a8722
Paul Masterson <pjmaster@mit.edu>
2015-01-06 11:48:45 -0500

    add Helipad

---
+++ basePlayer/Helipad.java
@@ -0,0 +1,16 @@
+package basePlayer;
+
+import battlecode.common.RobotController;
+
+public class Helipad extends Entity {
+
+	public static void run(RobotController rc) {
+		try {
+			//TODO: Helipad
+		} catch (Exception e) {
+			System.out.println("Helipad Exception");
+            e.printStackTrace();
+		}
+	}
+	
+}
