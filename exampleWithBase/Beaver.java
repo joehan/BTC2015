8a3e816d5130393d311fd688637e5f6b6a4ce886
Paul Masterson <pjmaster@mit.edu>
2015-01-05 22:10:59 -0500

    example player in our base format

---
+++ exampleWithBase/Beaver.java
@@ -0,0 +1,31 @@
+package exampleWithBase;
+
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+
+public class Beaver {
+
+	public static void run(RobotController rc) {
+		try {
+			if (rc.isWeaponReady()) {
+				Status.attackSomething(rc);
+			}
+			if (rc.isCoreReady()) {
+				int fate = Status.rand.nextInt(1000);
+				if (fate < 8 && rc.getTeamOre() >= 300) {
+					Status.tryBuild(Status.directions[Status.rand.nextInt(8)],RobotType.BARRACKS, rc);
+				} else if (fate < 600) {
+					rc.mine();
+				} else if (fate < 900) {
+					Status.tryMove(Status.directions[Status.rand.nextInt(8)], rc);
+				} else {
+					Status.tryMove(rc.senseHQLocation().directionTo(rc.getLocation()), rc);
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Beaver Exception");
+            e.printStackTrace();
+		}
+	}
+	
+}
---
+++ exampleWithBase/RobotPlayer.java
@@ -0,0 +1,39 @@
+package exampleWithBase;
+
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+
+public class RobotPlayer {
+	
+	public static void run(RobotController rc) {
+		Status.init(rc);
+		RobotType type = rc.getType();
+		while(true) {
+			
+			try {
+                rc.setIndicatorString(0, "This is an indicator string.");
+                rc.setIndicatorString(1, "I am a " + rc.getType());
+            } catch (Exception e) {
+                System.out.println("Unexpected exception");
+                e.printStackTrace();
+            }
+			
+			if(type == RobotType.HQ) {
+				HQ.run(rc);
+			} else if(type == RobotType.TOWER) {
+				Tower.run(rc);
+			} else if(type == RobotType.BASHER) {
+				Basher.run(rc);
+			} else if(type == RobotType.SOLDIER) {
+				Soldier.run(rc);
+			} else if(type == RobotType.BEAVER) {
+				Beaver.run(rc);
+			} else if(type == RobotType.BARRACKS) {
+				Barracks.run(rc);
+			}
+			
+			rc.yield();
+		}
+	}
+	
+}
