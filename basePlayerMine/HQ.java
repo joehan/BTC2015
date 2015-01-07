937f7e7293c762c1d486b8d330c08ee16d3ec30a
Ben Greenberg <bengreen@mit.edu>
2015-01-06 23:03:20 -0500

    miners

--- basePlayerMine/HQ.java
+++ basePlayerMine/HQ.java
@@ -8,14 +8,20 @@ public class HQ extends Entity {
 	public static void run(RobotController rc) {
 		while(true) {
 			try {
-				int fate = Status.rand.nextInt(10000);
-				countUnitsAndBroadcast(rc, rc.getTeam());
-				if (rc.isWeaponReady()) {
-					attackSomething(rc);
-				}
-
-				if (rc.isCoreReady() && rc.getTeamOre() >= 100 && fate < Math.pow(1.2,12-rc.readBroadcast(Status.numBeaverChannel))*10000) {
-					trySpawn(Status.directions[Status.rand.nextInt(8)], RobotType.BEAVER, rc);
+				if (rc.isCoreReady()) {
+					int fate = Status.rand.nextInt(10000);
+					countUnitsAndBroadcast(rc, rc.getTeam());
+					if (rc.isWeaponReady()) {
+						attackSomething(rc);
+					}
+	
+					if (rc.isCoreReady() 
+							&& rc.getTeamOre() >= 100 
+							&& fate < Math.pow(1.2,12-rc.readBroadcast(Status.numBeaverChannel))*10000 
+							&& (rc.readBroadcast(Status.numMinerFactoryChannel) < 1 
+									|| rc.readBroadcast(Status.numBeaverChannel) < 5)) {
+						trySpawn(Status.directions[Status.rand.nextInt(8)], RobotType.BEAVER, rc);
+					}
 				}
 			} catch (Exception e) {
 				System.out.println("HQ Exception");
--- basePlayerMine/MinerFactory.java
+++ basePlayerMine/MinerFactory.java
@@ -1,13 +1,19 @@
 package basePlayerMine;
 
 import battlecode.common.RobotController;
+import battlecode.common.RobotType;
 
 public class MinerFactory extends Entity {
 
 	public static void run(RobotController rc) {
 		while(true) {
 			try {
-				//TODO: MinerFactory
+				if (rc.isCoreReady()) {
+					int numFactories = rc.readBroadcast(Status.numMinerFactoryChannel);
+					if (numFactories >= 1 && rc.getTeamOre() >= RobotType.MINER.oreCost) {
+						trySpawn(Status.directions[Status.rand.nextInt(8)],RobotType.MINER, rc);
+					}
+				}
 			} catch (Exception e) {
 				System.out.println("MinerFactory Exception");
 	            e.printStackTrace();
