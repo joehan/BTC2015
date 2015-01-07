9d8669b3e2a486505e91583e0c6e3f53e82a8426
Paul Masterson <pjmaster@mit.edu>
2015-01-07 14:25:38 -0500

    supplyDrone

---
+++ supplyDrone/SupplyDepot.java
@@ -0,0 +1,19 @@
+package supplyDrone;
+
+import battlecode.common.RobotController;
+
+public class SupplyDepot extends Entity {
+
+	public static void run(RobotController rc) {
+		while(true) {
+			try {
+				//TODO: SupplyDepot
+			} catch (Exception e) {
+				System.out.println("SupplyDepot Exception");
+	            e.printStackTrace();
+			}
+			rc.yield();
+		}
+	}
+	
+}
