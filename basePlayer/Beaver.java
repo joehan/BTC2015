166e721b640262e717e58435fc11dda5f3cbc721
Ben Greenberg <bengreen@mit.edu>
2015-01-06 19:28:41 -0500

    check beaver core

--- basePlayer/Beaver.java
+++ basePlayer/Beaver.java
@@ -6,7 +6,9 @@ public class Beaver extends Entity {
 
 	public static void run(RobotController rc) {
 		try {
-			mine(rc);
+			if (rc.isCoreReady()) {
+				mine(rc);
+			}
 		} catch (Exception e) {
 			System.out.println("Beaver Exception");
             e.printStackTrace();
