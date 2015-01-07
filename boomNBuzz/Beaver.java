201d274f1f4d539bb333d73069fe7fa746e4ca90
Joe Hanley <jhanley@mit.edu>
2015-01-07 00:57:38 -0500

    Buzzing and booming, kinda

--- boomNBuzz/Beaver.java
+++ boomNBuzz/Beaver.java
@@ -1,4 +1,4 @@
-package missileAndDrones;
+package boomNBuzz;
 
 import java.util.Random;
 
@@ -17,22 +17,12 @@ public class Beaver extends Entity {
 					attackSomething(rc);
 				}
 				if (rc.isCoreReady()) {
-	//				int fate = rand.nextInt(1000);
-	//				if (fate < 8 && rc.getTeamOre() >= 300) {
-	//					tryBuild(directions[rand.nextInt(8)],RobotType.BARRACKS);
-	//				} else if (fate < 600) {
-	//					rc.mine();
-	//				} else if (fate < 900) {
-	//					tryMove(directions[rand.nextInt(8)]);
-	//				} else {
-	//					tryMove(rc.senseHQLocation().directionTo(rc.getLocation()));
-	//				}
 					MapLocation myLoc = rc.getLocation();
 					int mineFactories = rc.readBroadcast(Status.numMinerFactoryChannel);
 					int helipads = rc.readBroadcast(Status.numHelipadChannel);
 					int aerospaceLabs = rc.readBroadcast(Status.numAeroSpaceLabChannel);
 					double ore = rc.getTeamOre();
-					// build 5 depots, 1 helipad, 2 aerospace labs, then more aerospace labs if there is a surplus
+					// 
 					if (ore >= RobotType.MINERFACTORY.oreCost && mineFactories==0){ 
 						tryBuild(Status.directions[rand.nextInt(8)],RobotType.MINERFACTORY, rc);
 					} else if (ore >= 300 && helipads < 1 && mineFactories>0) {
@@ -46,9 +36,13 @@ public class Beaver extends Entity {
 		} catch (Exception e) {
 				System.out.println("Beaver Exception");
 	            e.printStackTrace();
-			}
+		}
+			
+		rc.yield();
+		
 		}
 	}
+	
 	
 	
 	
---
+++ boomNBuzz/Helipad.java
@@ -0,0 +1,26 @@
+package boomNBuzz;
+
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+
+public class Helipad extends Entity {
+
+	public static void run(RobotController rc) {
+		while(true) {
+			try {
+				int numDrones = rc.readBroadcast(Status.numDroneChannel);
+				int numLaunchers = rc.readBroadcast(Status.numLauncherChannel);
+				if(rc.isCoreReady() && (numDrones-numLaunchers) < 10) {
+					if(rc.getTeamOre() >= RobotType.DRONE.oreCost) {
+						trySpawn(Status.directions[Status.rand.nextInt(8)], RobotType.DRONE, rc);
+					}
+				}
+			} catch (Exception e) {
+				System.out.println("Helipad Exception");
+	            e.printStackTrace();
+			}
+			rc.yield();
+		}
+	}
+	
+}
